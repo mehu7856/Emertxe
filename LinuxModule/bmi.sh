28ede62d989d65809b10a064af55727de7fff15e
Mahaboob Basha <84396416+mehu7856@users.noreply.github.com>
2021-12-05 10:36:06 +0530

    Update bmi.sh

--- LinuxModule/bmi.sh
+++ LinuxModule/bmi.sh
@@ -12,13 +12,13 @@ regex='^[+-]?[0-9]?+[.]?[0-9]?+$'
 read -p "the enter number weight in kg:" weight
 read -p "the enter number height in mm:" height
 
-if [[ $weight =~ $regex ]]                                                                                                   #Use regex representing string matching patterns
+if [[ $weight =~ $regex ]]                                                   #Use regex representing string matching patterns
 then
 	if [[ $height =~ $regex ]]
 	then
-	product=`echo "$height*$height" |bc`                                                                 # product=height*height
+	product=`echo "$height*$height" |bc`                                # product=height*height
 
-	bmi=`echo "scale=2;$weight/$product"|bc`                                                          #bmi=weight/height
+	bmi=`echo "scale=2;$weight/$product"|bc`                            #bmi=weight/height
 	
 		if [ `echo "$bmi < 18.5"|bc` -eq 1 ]                        #if ur weight is lessthan 18.5  print under weight
 		then
@@ -27,7 +27,7 @@ then
 		if [ `echo "$bmi >= 18.5"|bc` -eq 1 ]                                                  
 		then
 
-			if [ `echo "$bmi <= 24.5"|bc` -eq 1 ]                    #if ur weight is lessthan 24.5 print the your normal     
+			if [ `echo "$bmi <= 24.5"|bc` -eq 1 ]                 #if ur weight is lessthan 24.5 print the your normal     
 			then
 				echo "you are normal"
 			fi
@@ -35,12 +35,12 @@ then
 			if [ `echo "$bmi >= 25"|bc` -eq 1 ]
 			then
 
-				if [ `echo "$bmi <= 29.8"|bc` -eq 1  ]                #if ur weigth is lessthan 29.8 print over weigth
+				if [ `echo "$bmi <= 29.8"|bc` -eq 1  ]         #if ur weigth is lessthan 29.8 print over weigth
 				then 
 					echo "you are overweighted"
 				fi
 			fi
-		if [ `echo "$bmi > 30 " |bc` -eq 1 ]                          #if ur weigth is gretarthan 30 print Ur obese
+		if [ `echo "$bmi > 30 " |bc` -eq 1 ]                           #if ur weigth is gretarthan 30 print Ur obese
 		then
 			echo "you are obese"
 		fi
