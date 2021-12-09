adb7ece251e5bee3f713efa36147198cbca29d7f
Mahaboob Basha <84396416+mehu7856@users.noreply.github.com>
2021-12-09 18:41:13 +0530

    Update

---
+++ LinuxModule/add_two_numbers.sh
@@ -0,0 +1,33 @@
+<< COMMENT
+
+Name : Mahaboob basha S.R
+
+Date : 15-10-2021
+
+Description :  script for addition of two numbers for real numbers also
+
+Sample input : 10 20
+
+Sample output : 30
+
+COMMENT
+
+#!/bin/bash
+
+regex='^[0-9]+[.]?[0-9]+$'                                              
+
+read -p "enter a numbers :" num1 num2                       
+
+sum=`echo "$num1 + $num2"| bc`                                  
+
+if [[ $sum =~ $regex ]]                                                       
+
+then
+
+        echo addition "$sum"                                                     
+
+else
+
+        echo "please enter only integers or real numbers"           
+
+fi     
